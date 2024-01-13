be2420399a3a184f9df5f2a0fbf3ea20d86c2064
Prithesh Salian <pritheshmarodi@gmail.com>
2024-01-13 20:25:28 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { DetailsComponent } from './details/details.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, MenuComponent,DetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
